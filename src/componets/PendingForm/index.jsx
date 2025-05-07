9965969c851063c03a69e74d7ed2a9e5b89c37e5
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-05-07 00:17:12 -0300

    ✅ Feature: Habilitando botão  check para envio do form e atualizando pagina com novo report

--- src/componets/PendingForm/index.jsx
+++ src/componets/PendingForm/index.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
-import { 
-  FaCog, 
-  FaTrash, 
-  FaRegArrowAltCircleLeft, 
-  FaRegPlusSquare, 
+import React, { useEffect, useRef } from 'react'
+import {
+  FaCog,
+  FaTrash,
+  FaRegArrowAltCircleLeft,
+  FaRegPlusSquare,
   FaRegCheckCircle,
   FaRegMinusSquare,
 } from 'react-icons/fa'
@@ -21,10 +21,10 @@ import { Spin } from '../Spin'
 import { usePendingFormState } from '../../hooks/usePendingFormState'
 import { useSupabaseReports } from '../../hooks/useSupabaseReports'
 
-
 const PendingForm = ({ selectTech, selectedDate, onClose }) => {
   const { state, dispatch } = usePendingFormState()
   const { relatorios, deleteReport, addReport } = useSupabaseReports()
+  const formRef = useRef(null)
 
   useEffect(() => {
     if (selectedDate) {
@@ -38,18 +38,18 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
       dispatch({ type: 'SET_FIELD', field: 'tecnico', value: selectTech })
     }
   }, [selectTech])
- 
+
   const applyFilters = () => {
     return relatorios.filter((relatorio) => {
       const createdDate = relatorio.data_atendimento
-      const inicioOk = !state.filterDataInicio || createdDate >= state.filterDataInicio
+      const inicioOk =
+        !state.filterDataInicio || createdDate >= state.filterDataInicio
       const fimOk = !state.filterDataFim || createdDate <= state.filterDataFim
-      const matchData = createdDate === new Date(state.data).toISOString().split('T')[0]
+      const matchData =
+        createdDate === new Date(state.data).toISOString().split('T')[0]
       return inicioOk && fimOk && matchData
     })
   }
-  
-  
 
   const formatDate = (date) => {
     const options = {
@@ -65,41 +65,46 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
 
   const handleSaveData = () => {
     dispatch({ type: 'SET_FIELD', field: 'data', value: state.data })
-    dispatch({ type: 'SET_FIELD', field: 'editandoData', value: state.editandoData })
+    dispatch({
+      type: 'SET_FIELD',
+      field: 'editandoData',
+      value: state.editandoData,
+    })
   }
 
   const sendMessageOnWhatsapp = async () => {
     dispatch({ type: 'SET_LOADING', value: true })
     try {
-      const response = await fetch("http://192.168.15.65:8080/message/sendText/Plantao", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "apikey": "lo593kofj2akb2m9vxl9w"
-        },
-        body: JSON.stringify({
-          number: "120363025885372734@g.us",
-          options: {
-            delay: 1200,
-            presence: "composing",
-            linkPreview: false
+      const response = await fetch(
+        'http://192.168.15.65:8080/message/sendText/Plantao',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            apikey: 'lo593kofj2akb2m9vxl9w',
           },
-          textMessage: {
-            text: `⚠ Plantão de: ${state.tecnico}\nLocal: ${state.local}\nPENDÊNCIA: *${state.pendencia}*`
-          }
-          
-        })
-      });
-  
-      const data = await response.json();
-      console.log("Resposta da API:", data);
+          body: JSON.stringify({
+            number: '120363025885372734@g.us',
+            options: {
+              delay: 1200,
+              presence: 'composing',
+              linkPreview: false,
+            },
+            textMessage: {
+              text: `⚠ Plantão de: ${state.tecnico}\nLocal: ${state.local}\nPENDÊNCIA: *${state.pendencia}*`,
+            },
+          }),
+        },
+      )
+
+      const data = await response.json()
+      console.log('Resposta da API:', data)
     } catch (error) {
-      console.error("Erro ao enviar mensagem:", error);
+      console.error('Erro ao enviar mensagem:', error)
     } finally {
       dispatch({ type: 'SET_LOADING', value: false })
     }
-  };
-
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -112,26 +117,29 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
       local: state.local,
       responsavel: state.responsavel,
       hora_ini: state.horaInicio,
-      hora_fini: state.horaFinal,  
+      hora_fini: state.horaFinal,
       pendencias: state.gerouPendencia ? state.pendencia : '',
     }
-    addReport(newReport)
-    // if (state.gerouPendencia && state.pendencia.trim() !== '') {
-    //   await sendMessageOnWhatsapp()
-    // }
-    dispatch({ type: 'RESET' })
+    await addReport(newReport)
     dispatch({ type: 'SET_FIELD', field: 'tecnico', value: selectTech })
-    window.location.reload();
+    dispatch({
+      type: 'SET_FIELD',
+      field: 'data',
+      value: new Date(selectedDate).toISOString().split('T')[0],
+    })
+    dispatch({ type: 'SET_OPENFORM' })
   }
 
+  // if (state.gerouPendencia && state.pendencia.trim() !== '') {
+  //   await sendMessageOnWhatsapp()
+  // }
   return (
     <Container>
       <Title>
         Plantão Referente ao dia: {formatDate(state.data)}
         <FaCog
           style={{ marginLeft: 10, cursor: 'pointer' }}
-          onClick={() => dispatch({ type: 'TOGGLE_EDIT_DATE' })
-        }
+          onClick={() => dispatch({ type: 'TOGGLE_EDIT_DATE' })}
         />
       </Title>
 
@@ -147,8 +155,8 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
                 value: e.target.value,
               })
             }
-          />      
-          <button onClick={handleSaveData}>Salvar</button>
+          />
+          <button>Salvar</button>
         </div>
       )}
 
@@ -163,14 +171,18 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
               : 'R$ 80,00'}
       </h2>
       <h2>Técnico: {selectTech}</h2>
-      <p style={{ marginTop: 20 }}>____________________________________________</p>
+      <p style={{ marginTop: 20 }}>
+        ____________________________________________
+      </p>
 
-      {state.abrirFormulario && (
-        state.loading ? <Spin /> :
-        <form onSubmit={handleSubmit}>
-          <Content>
-          <label>Data do chamado</label>
-          <Input
+      {state.abrirFormulario &&
+        (state.loading ? (
+          <Spin />
+        ) : (
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <Content>
+              <label>Data do chamado</label>
+              <Input
                 type="date"
                 placeholder="Data Início"
                 value={state.data}
@@ -182,112 +194,130 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
                   })
                 }}
               />
-          </Content>
-          <Content>
-          <label>Local do chamado</label>
-            <Input
-              placeholder="Local do Chamado:"
-              type="text"
-              value={state.local}
-              onChange={(e) =>
-                dispatch({
-                  type: 'SET_FIELD',
-                  field: 'local',
-                  value: e.target.value,
-                })
-              }
-            />
-          </Content>
-          <Content>
-          <label>Contato do chamado</label>
-            <Input
-              placeholder="Responsável do Contato:"
-              type="text"
-              value={state.responsavel}
-              onChange={(e) =>
-                dispatch({
-                  type: 'SET_FIELD',
-                  field: 'responsavel',
-                  value: e.target.value,
-                })
-              }
-            />
-          </Content>
-          <Content>
-          <label>Hora de início</label>
-            <Input
-              placeholder="Hora de Início:"
-              type="time"
-              value={state.horaInicio}
-              onChange={(e) =>
-                dispatch({
-                  type: 'SET_FIELD',
-                  field: 'horaInicio',
-                  value: e.target.value,
-                })
-              }
-            />
-          </Content>
-          <Content>
-          <label>Hora final</label>
-            <Input
-              placeholder="Hora Final:"
-              type="time"
-              value={state.horaFinal}
-              onChange={(e) =>
-                dispatch({
-                  type: 'SET_FIELD',
-                  field: 'horaFinal',
-                  value: e.target.value,
-                })
-              }
-            />
-          </Content>
-          <Content>
-            <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
-              <CheckBox
-                type="checkbox"
-                checked={state.gerouPendencia}
-                onChange={() => dispatch({ type: 'TOGGLE_PENDENCIA' })}
-              />
-              Gerou Pendência ?
-            </div>
-          </Content>
-          {state.gerouPendencia && (
+            </Content>
             <Content>
-              <TextArea
-                placeholder="Descrição da Pendência:"
-                value={state.pendencia}
+              <label>Local do chamado</label>
+              <Input
+                placeholder="Local do Chamado:"
+                type="text"
+                value={state.local}
                 onChange={(e) =>
                   dispatch({
                     type: 'SET_FIELD',
-                    field: 'pendencia',
+                    field: 'local',
                     value: e.target.value,
                   })
                 }
               />
             </Content>
-          )}
-          <button type="submit">Salvar Relatório</button>
-        </form>
-      )}
+            <Content>
+              <label>Contato do chamado</label>
+              <Input
+                placeholder="Responsável do Contato:"
+                type="text"
+                value={state.responsavel}
+                onChange={(e) =>
+                  dispatch({
+                    type: 'SET_FIELD',
+                    field: 'responsavel',
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Content>
+            <Content>
+              <label>Hora de início</label>
+              <Input
+                placeholder="Hora de Início:"
+                type="time"
+                value={state.horaInicio}
+                onChange={(e) =>
+                  dispatch({
+                    type: 'SET_FIELD',
+                    field: 'horaInicio',
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Content>
+            <Content>
+              <label>Hora final</label>
+              <Input
+                placeholder="Hora Final:"
+                type="time"
+                value={state.horaFinal}
+                onChange={(e) =>
+                  dispatch({
+                    type: 'SET_FIELD',
+                    field: 'horaFinal',
+                    value: e.target.value,
+                  })
+                }
+              />
+            </Content>
+            <Content>
+              <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+                <CheckBox
+                  type="checkbox"
+                  checked={state.gerouPendencia}
+                  onChange={() => dispatch({ type: 'TOGGLE_PENDENCIA' })}
+                />
+                Gerou Pendência ?
+              </div>
+            </Content>
+            {state.gerouPendencia && (
+              <Content>
+                <TextArea
+                  placeholder="Descrição da Pendência:"
+                  value={state.pendencia}
+                  onChange={(e) =>
+                    dispatch({
+                      type: 'SET_FIELD',
+                      field: 'pendencia',
+                      value: e.target.value,
+                    })
+                  }
+                />
+              </Content>
+            )}
+          </form>
+        ))}
       <div style={{ display: 'flex', gap: 10, margin: 20 }}>
-        <FaRegArrowAltCircleLeft style={{ width: 40, height: 40 }} onClick={onClose} />
-        {
-          state.abrirFormulario ?
-          <FaRegMinusSquare style={{ width: 40, height: 40, cursor: 'pointer' }} onClick={() => dispatch({ type: 'SET_OPENFORM' })} />:
-          <FaRegPlusSquare style={{ width: 40, height: 40, cursor: 'pointer' }} onClick={() => dispatch({ type: 'SET_OPENFORM' })} />
-        }
-        <FaRegCheckCircle style={{ width: 40, height: 40 }} />
+        <FaRegArrowAltCircleLeft
+          style={{ width: 40, height: 40 }}
+          onClick={onClose}
+        />
+        {state.abrirFormulario ? (
+          <FaRegMinusSquare
+            style={{ width: 40, height: 40, cursor: 'pointer' }}
+            onClick={() => dispatch({ type: 'SET_OPENFORM' })}
+          />
+        ) : (
+          <FaRegPlusSquare
+            style={{ width: 40, height: 40, cursor: 'pointer' }}
+            onClick={() => dispatch({ type: 'SET_OPENFORM' })}
+          />
+        )}
+        <FaRegCheckCircle
+          style={{
+            width: 40,
+            height: 40,
+            cursor: state.abrirFormulario ? 'pointer' : 'not-allowed',
+            color: state.abrirFormulario ? '#28a745' : '#ccc', // verde ativo, cinza desativado
+          }}
+          onClick={state.abrirFormulario ? handleSubmit : undefined}
+        />
       </div>
 
       <GridRelatorio className="relatorios-grid">
         {applyFilters().map((relatorio, index) => (
-          <div key={index} >
-              <CardPlantao key={index} className="relatorio-card">
+          <div key={index}>
+            <CardPlantao key={index} className="relatorio-card">
               <div>
                 <p>
-                  <strong>{formatDate(relatorio.data_atendimento.slice(0,10))}</strong>
+                  <strong>
+                    {formatDate(relatorio.data_atendimento.slice(0, 10))}
+                  </strong>
                 </p>
                 <p>
                   <strong>Técnico:</strong> {relatorio.tecnico}
@@ -311,12 +341,12 @@ const PendingForm = ({ selectTech, selectedDate, onClose }) => {
                 )}
               </div>
               <div>
-              <FaTrash
-                style={{ cursor: 'pointer', color: 'red' }}
-                onClick={() => deleteReport(relatorio.id)}
+                <FaTrash
+                  style={{ cursor: 'pointer', color: 'red' }}
+                  onClick={() => deleteReport(relatorio.id)}
                 />
               </div>
-                </CardPlantao>              
+            </CardPlantao>
           </div>
         ))}
       </GridRelatorio>
