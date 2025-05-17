2f8252eb74cc4de49ac76b358dc25b8fa1690c75
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-05-17 06:51:50 -0300

    ✅ Criando  .env para chaves e refatorando o createClient do Supabase

--- src/componets/Spin/index.jsx
+++ src/componets/Spin/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { SpinnerContainer, SpinningImage } from './style';
+import React from 'react'
+import { SpinnerContainer, SpinningImage } from './style'
 
 const Spin = () => {
   return (
     <SpinnerContainer>
       <SpinningImage src="/Logo_Symbil_Blue.png" alt="Carregando..." />
     </SpinnerContainer>
-  );
-};
+  )
+}
 
-export { Spin };
+export { Spin }
