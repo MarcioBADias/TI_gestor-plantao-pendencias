8dcc281ee086a0df76be5a45a4ed3e72b5789eee
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2025-05-29 15:25:39 -0300

    Ajuste de dimenções

--- src/pages/GoogleSheetPage/index.jsx
+++ src/pages/GoogleSheetPage/index.jsx
@@ -33,8 +33,12 @@ const GoogleSheetPage = () => {
   return (
     <div>
       <h2>Dados da planilha</h2>
-      <div style={{ width: '90%', height: '90%'}}>
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQETzR_zkC8rWwxGyOJYCwGMK4eK-AOvzDrAFAaLfAjUYmc42hLGw5YzMg4NbUvr4_l6qkOTDdiwksg/pubhtml?gid=297320331&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      <div>
+        <iframe 
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQETzR_zkC8rWwxGyOJYCwGMK4eK-AOvzDrAFAaLfAjUYmc42hLGw5YzMg4NbUvr4_l6qkOTDdiwksg/pubhtml?gid=297320331&amp;single=true&amp;widget=true&amp;headers=false"
+          style={{ width: '90%', height: '90%'}}
+          >
+        </iframe>
       </div>
       <ul>
         {data.map((item, index) => (
