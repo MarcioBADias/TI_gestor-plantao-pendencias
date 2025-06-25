e69056f3ebd47cbda90c2560ab81dea2363581cd
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-06-25 11:38:24 -0300

    ✅ Feature: icone de download do link e copia e cola do mesmo.

--- src/pages/InfoUteisPage/index.jsx
+++ src/pages/InfoUteisPage/index.jsx
@@ -1,38 +1,74 @@
 import { useState } from 'react'
 import { AccordionItem } from '../../componets/AccordionItem'
-import { InfoContainer, SubItem, Title } from './style'
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
+import { CopyStatus, InfoContainer, LinkContainer, SubItem, SubItemContainer, SubTitle, Title } from './style'
+import { FaChevronDown, FaChevronUp, FaDownload, FaLink } from 'react-icons/fa'
 
 const InfoUteisPage = () => {
+  const [copiedStatus, setCopiedStatus] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
+
+  const centralizadorLink =
+    'https://drive.usercontent.google.com/download?id=1hWLTnJOD8RCvNc4SJXbBB4MQrC3LPVn-&export=download&authuser=0'
+  
+  const virtualBoxLink = 'https://download.virtualbox.org/virtualbox/7.1.10/VirtualBox-7.1.10-169112-Win.exe'
   
     const toggleOpen = () => {
       setIsOpen(!isOpen)
     }
+
+  const handleCopyLink = async (link) => {
+    try {
+      await navigator.clipboard.writeText(link)
+      setCopiedStatus(true)
+      setTimeout(() => {
+        setCopiedStatus(false)
+      }, 2000)
+    } catch (err) {
+      console.error('Falha ao copiar o link: ', err)
+    }
+  }
+
+  const handleDownloadLink = (link) => {
+    window.open(link, '_blank')
+  }
+
   return (
     <InfoContainer>
       <Title>Informações Úteis</Title>
 
       <AccordionItem title="PDV Legal">
         <div>
-          <SubItem onClick={toggleOpen} style={{ display: 'flex', justifyContent: 'space-between' }} > 
-        <p>
-          Centralizador
-        </p>
-        {isOpen ? <FaChevronUp /> : <FaChevronDown />}
+          <SubItem
+            onClick={toggleOpen}
+          >
+            <SubTitle>Centralizador</SubTitle>
+        {isOpen ? <FaChevronUp style={{ color: 'var(--color-primary)' }} /> : <FaChevronDown style={{ color: 'var(--color-primary)' }} />}
           </SubItem>
-          {isOpen &&  
-          <div>
-          <p>Link para baixar o centralizador</p>
-          <p>
-            "https://drive.usercontent.google.com/download?id=1hWLTnJOD8RCvNc4SJXbBB4MQrC3LPVn-&export=download&authuser=0"
-          </p>
-          </div>
-}
-           </div>
-
-        
-        
+          <SubItemContainer >
+            {isOpen &&
+            <LinkContainer>
+              <p>Clique para copiar a URL de douload ou baixar o <pan style={{ color: 'var(--color-primary)' }}>centralizador</pan></p>
+              <CopyStatus isVisible={copiedStatus}>Copiado</CopyStatus>
+              <FaLink
+                style={{ cursor: 'pointer', color: 'var(--color-primary)' }}
+                onClick={() => handleCopyLink(centralizadorLink)}
+              />
+              <FaDownload onClick={() => handleDownloadLink(centralizadorLink)} style={{ cursor: 'pointer', color: 'var(--color-primary)' }}/>
+            </LinkContainer>}
+          </SubItemContainer>
+          <SubItemContainer >
+            {isOpen &&
+            <LinkContainer>
+              <p>Clique para copiar a URL de douload ou baixar o <pan style={{ color: 'var(--color-primary)' }}>Virtual Box</pan></p>
+              <CopyStatus isVisible={copiedStatus}>Copiado</CopyStatus>
+              <FaLink
+                style={{ cursor: 'pointer', color: 'var(--color-primary)' }}
+                onClick={() => handleCopyLink(virtualBoxLink)}
+              />
+              <FaDownload onClick={() => handleDownloadLink(virtualBoxLink)} style={{ cursor: 'pointer', color: 'var(--color-primary)' }}/>
+            </LinkContainer>}
+          </SubItemContainer>
+        </div>
       </AccordionItem>
 
       
--- src/pages/InfoUteisPage/style.jsx
+++ src/pages/InfoUteisPage/style.jsx
@@ -14,10 +14,38 @@ export const Title = styled.h1`
 `
 
 export const SubItem = styled.div`
+  border-bottom: 2px var(--color-primary) solid;
+  display: flex;
+  justify-content: space-between;
   margin-bottom: 0.5rem;
-  color: #333;
+  padding: 0.5rem;
 
   &:last-child {
     margin-bottom: 0;
   }
+`
+
+export const SubTitle = styled.p`
+  color: var(--color-primary);
+  font-weight: bold;
+`
+
+export const SubItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`
+
+export const CopyStatus = styled.span`
+  color: green;
+  margin-left: 10px;
+  font-weight: bold;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transition: opacity 0.5s ease-in-out;
+`
+
+export const LinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem; 
 `
